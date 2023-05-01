44eefec88a12ef749cd64e877ce61fe7fc3f554c
Patrick <patrick@godknows.de>
2023-05-01 19:10:12 +0200

    Add project files.

---
+++ ChatClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+var connection = new HubConnectionBuilder()
+        .WithUrl("https://localhost:7160/chathub")
+        .Build();
+
+connection.On<string, string>("ReceiveMessage", (user, message) =>
+{
+    Console.WriteLine($"{user}: {message}");
+});
+
+await connection.StartAsync();
+
+Console.WriteLine("Connected. Enter your name:");
+var userName = Console.ReadLine();
+
+while (true)
+{
+    var message = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(message))
+    {
+        break;
+    }
+
+    await connection.SendAsync("SendMessage", userName, message);
+}
+
+await connection.DisposeAsync();
